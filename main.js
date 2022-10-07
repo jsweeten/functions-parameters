cf88dc61e360bf9f04b739eb08892f5215aa7fe3
Justin Sweeten <jtsweeten@gmail.com>
2022-10-07 09:45:36 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (inputNum) => {
+    const outputNum = inputNum / 4
+    return outputNum
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(600)
+
+// Log the return value
+console.log(result)
